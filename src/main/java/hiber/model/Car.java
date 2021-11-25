b120f9825e361ccdaf108fb87205c463f6a5ec23
Artemiy Yahev <90742524+YaSlawaMarlow@users.noreply.github.com>
2021-11-26 04:37:35 +0600

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,62 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "model")
+    String model;
+
+    @Column(name = "series")
+    int series;
+
+    @OneToOne(mappedBy = "car")
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Car() {
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
